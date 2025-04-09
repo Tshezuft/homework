7b8dc9406c4d87ffd671ef2228369c12cf549775
Tshezuft <mixnikitin11@gmail.com>
2025-04-09 19:24:56 +0300

    Add files via upload

---
+++ pink 2/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const aussiBtn = document.getElementById("Aussi_btn");
+const aussi = document.getElementById("Aussi");
+console.log(aussi, aussiBtn);
+aussiBtn.addEventListener('onclick', function(){
+aussi.style.background("#fff");
+})
