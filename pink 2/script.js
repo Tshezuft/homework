ff7780f4d6ae71c14857ee1aeb313bf17f4c1e3f
Tshezuft <mixnikitin11@gmail.com>
2025-04-09 19:34:48 +0300

    Delete pink 2 directory

--- pink 2/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const aussiBtn = document.getElementById("Aussi_btn");
-const aussi = document.getElementById("Aussi");
-console.log(aussi, aussiBtn);
-aussiBtn.addEventListener('onclick', function(){
-aussi.style.background("#fff");
-})
