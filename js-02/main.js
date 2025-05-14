4a6f54636ca6aef076be3214ff86f828ed8475a2
Tshezuft <mixnikitin11@gmail.com>
2025-05-14 17:37:23 +0300

    Add files via upload

---
+++ js-02/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let btnOpen = document.querySelector('.btn-open');
+let modal = document.querySelector('.modal');
+let btnClose = document.querySelector('.modal__close-btn');
+const body = document.body;
+
+const closeModal = () =>{
+    modal.classList.remove('modal--open');
+    body.classList.add('body--fixed')
+}
+
+const openModal = () =>{
+    modal.classList.add('modal--open');
+    body.classList.remove('body--fixed')
+
+}
+
+
+
+btnOpen.addEventListener('click', openModal)
+
+modal.addEventListener('click', event =>{
+    const target = event.target;
+    if (target && target.classList.contains('modal') || target.classList.contains('modal__close-btn')){
+        closeModal()
+    }
+})
+
+document.addEventListener('keydown', event =>{
+    if (event.code === 'Escape' && modal.classList.contains('modal--open')){
+        closeModal()
+    }
+})
