1c1b1cf51cd1979f2ed15169111860d6573ff8c1
Tshezuft <mixnikitin11@gmail.com>
2025-06-09 20:50:32 +0300

    Update main.js

--- WakeBake/js/main.js
+++ WakeBake/js/main.js
@@ -155,6 +155,13 @@
         }
     });
 
+    // mask for phone
+
+    const telInputs = document.querySelectorAll('input[type="tel"]');
+
+    const im = new Inputmask('+7 (999) 999-99-99');
+    im.mask(telInputs);
+
 
 
 
